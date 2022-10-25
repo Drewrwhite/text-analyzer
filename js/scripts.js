97518c600ab3a66b9f6bed81d6fdeee3a8906392
Drew White <d.white0002@gmail.com>
2022-10-25 16:27:42 -0700

    added changes to logic and html
    Co-authored-by: Chloe Le <yenchi.le1199@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
+// Utility Logic
+
+function isEmpty(testString) {
+  return (testString.trim().length === 0);
+}
+
 // Business Logic
 
 function wordCounter(text) {
-  if (text.trim().length === 0) {
+  if (isEmpty(text)) {
     return 0;
   }
   let wordCount = 0;
   const textArray = text.split(" ");
-  textArray.forEach(function (element) {
+  textArray.forEach(function(element) {
     if (!Number(element)) {
       wordCount++;
     }
@@ -15,21 +21,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  const textArray = text.split(" ");
-  let wordCount = 0;
-  textArray.forEach(function (element) {
-    if (word.toLowerCase() === element.toLowerCase()) {
-      wordCount++;
-    }
-  });
-  return wordCount;
-}
-
-
-
-
-function numberOfOccurrencesInText(word, text) {
-  if (word.trim().length === 0) {
+  if (isEmpty(word)) {
     return 0;
   }
   const textArray = text.split(" ");
@@ -44,6 +36,27 @@ function numberOfOccurrencesInText(word, text) {
 
 // UI Logic
 
+function boldPassage(word, text) {
+  if (isEmpty(word) || isEmpty(text)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      p.append(" ");
+    }
+  });
+  return p;
+}
+
 function handleFormSubmission() {
   event.preventDefault();
   const passage = document.getElementById("text-passage").value;
@@ -52,7 +65,6 @@ function handleFormSubmission() {
   const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
   document.getElementById("total-count").innerText = wordCount;
   document.getElementById("selected-count").innerText = occurrencesOfWord;
-  // new lines here!
   let boldedPassage = boldPassage(word, passage);
   if (boldedPassage) {
     document.querySelector("div#bolded-passage").append(boldedPassage);
@@ -64,6 +76,11 @@ function handleFormSubmission() {
 window.addEventListener("load", function() {
   document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
 });
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
+
+
 // function omitPunctuation(text) {
 //   const punctuation = [",", ".", "!", "?"];
 //   let newWord = text.split("");
